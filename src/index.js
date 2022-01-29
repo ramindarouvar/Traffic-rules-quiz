be56265ca59b6b1fe84c513994ec775d7a8ef44f
ramindarouvar <87871862+ramindarouvar@users.noreply.github.com>
2022-01-29 19:48:41 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from "react-redux";
 import App from "./components/containers/App";
 import { store } from './store/index';
+import './style.scss'
 
 render(
   <Provider store={store}>
