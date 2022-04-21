c3583f2bafd92633ebe076679363cb0de13ef197
Your Name <you@example.com>
2022-04-21 10:58:15 +0430

    change style of buttons and slider

--- src/components/quiz/QuizNav.jsx
+++ src/components/quiz/QuizNav.jsx
@@ -10,35 +10,36 @@ const QuizNav = ({onQuestionChange, questionId}) => {
                            const difference = e.target.value - questionId
                            // render the question seleted from range input 
                            onQuestionChange(difference);
-                            }
+                            }   
                         }
                 />
             </div>
-            <div className="btn-group">
-                <button type="submit" id="next-q" 
-                        className={questionId === 30 ? "q-nav-botton btn btn-success" : "q-nav-botton btn btn-primary"}
-                        onClick={() => {
-                            // Send +1 to the method to change the question method to go to the next question  
-                            onQuestionChange(1);
-                        }}
-                >
-                        <i className="fa fa-arrow-right" aria-hidden="true"></i>{" "}
-                        <span className="q-nav-text mr-2">{questionId === 30 ? "نتیجه" : "بعدی"}</span>{" "}
-                </button>
+            <div className="btn-group row justify-content-around" style={{width: "30%"}}>
+
                 {questionId===1 ? 
                     // Do not display the Go to "Previous" button when the user is on the first question.
                     null
                     :
-                    <button type="submit" id="back-q" className="q-nav-botton btn btn-primary"  
-                    onClick={() => {
-                        // Send -1 to the method to change the question method to go to the previous question
-                        onQuestionChange(-1)
-                    }}
+                    <button type="submit" id="back-q" className="q-nav-botton btn btn-primary col-6"  
+                            onClick={() => {
+                                // Send -1 to the method to change the question method to go to the previous question
+                                onQuestionChange(-1)
+                            }}
                     >   
-                        <span className="q-nav-text ml-3 active">قبلی</span>
-                        <i className="fa fa-arrow-left" aria-hidden="true"></i>
+                        <span className="q-nav-text active pr-1">قبلی</span>
+                        <i className="fa fa-arrow-right d-block" aria-hidden="true"></i>{" "}
                     </button>
                 }
+                    <button type="submit" id="next-q" 
+                        className={questionId === 30 ? "q-nav-botton btn btn-success col-6" : "q-nav-botton btn btn-primary col-6"}
+                        onClick={() => {
+                            // Send +1 to the method to change the question method to go to the next question  
+                            onQuestionChange(1);
+                        }}
+                    >
+                        <span className="q-nav-text pl-1">{questionId === 30 ? "نتیجه" : "بعدی"}</span>{" "}
+                        <i className="fa fa-arrow-left d-block" aria-hidden="true"></i>
+                    </button>
             </div>
         </div>
      );
